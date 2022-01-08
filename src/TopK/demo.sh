6fb3ab35b51436b8d9d0c6be70232c1c71b18735
sliding-sketch <41382295+sliding-sketch@users.noreply.github.com>
2022-01-08 22:42:46 +0800

    Add files via upload

---
+++ src/TopK/demo.sh
@@ -0,0 +1,7 @@
+g++ main.cpp -o main -std=c++11
+./main 1024 ../../dataset/sof.dat
+./main 1280 ../../dataset/sof.dat
+./main 1536 ../../dataset/sof.dat
+./main 1792 ../../dataset/sof.dat
+./main 2048 ../../dataset/sof.dat
+
