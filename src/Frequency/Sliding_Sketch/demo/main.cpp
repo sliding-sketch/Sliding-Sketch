c4dfae71c2322c566d562b1880b9ba0da1fccbbf
sliding-sketch <41382295+sliding-sketch@users.noreply.github.com>
2024-03-06 19:50:10 +0800

    Add files via upload

--- src/Frequency/Sliding_Sketch/demo/main.cpp
+++ src/Frequency/Sliding_Sketch/demo/main.cpp
@@ -6,86 +6,75 @@
 #include <unordered_map>
 #include <unordered_set>
 #include <cmath>
-#include "../MMap.h"
 #include "../clock.cpp"
 #include "../data.cpp"
 #include "../hash_class.cpp"
 
-#define tpTest 1
+//#define tpTest 1   // when set this tag, the program will run speed test, where it will only process items with a certain algorithm but will not stop and compute error at checkpoints
 
 using namespace std;
 unordered_map<Data, int, My_Hash> mp;
 unordered_set<Data, My_Hash> st;
 
-//argv[1]:cycle
-//argv[2]:hash_number
-//argv[3]:Memory
-//argv[4]:file
-//argv[5]:input_num_max
-//argv[6]:out_file
-//argv[7]:out_model
-//argv[8]:label_name
-//argv[9]:field
 
 void Read_File(int argc,char*argv[]){
     
     int hash_number = 5;
-    // cout << argv[0];
-    double mymemory  = atof(argv[1]);
-    cout << mymemory;
-    int input_num_max = 1000000*8;
-    int field_num = 3;
+    double mymemory  = 5; // size of memory usage, use unit MB
+    cout <<"memory "<<mymemory<<" MB"<<endl;;
+    int field_num = 3; // number of field in each bucket
     int row_length = (mymemory * 1024 * 1024) / hash_number / (4 * field_num);
 
 
-    // Data *dat = new Data[cycle + 1];
 
     unsigned long long int num = 0;
+    int Test_num = 0;
     double CM_dif = 0, CU_dif = 0, CO_dif = 0;
     double CM_ae = 0,  CU_ae = 0,  CO_ae = 0;
     double CM_re = 0,  CU_re = 0,  CO_re = 0;
-    int Test_num = 0;
-    LoadResult ret = Load(argv[2]);
-    // FILE* file = fopen(argv[2],"rb");
+    
+	FILE* file = fopen("../../../../data/formatted00.dat","rb");
     Data packet;
-    Data* indat = (Data*)(ret.start);
+    vector<Data> indat;
+    unsigned int latestT = 0;
+    while(fread(packet.str, DATA_LEN, 1, file) > 0)
+    {
+    	packet.timestamp = indat.size(); // as the dataset provided in the demo has no timestamp, we use count-based sliding window. In other words, each item will get a timestamp equal to its index in the datasteam, and the length of the
+		// sliding window is equal to the number of items in it. 
+    	indat.push_back(packet);
+	}
+	unsigned int input_num = indat.size();
     queue<Data> dat;
 
-    int cycle = (indat[input_num_max -1].timestamp - indat[0].timestamp ) / 8;
+    int cycle = 200000; // the length of the sliding window, here we use count based sliding window. 
+    cout<<"window size "<<cycle<<endl;
+    
     Recent_Counter CM_Counter(cycle, hash_number * row_length, row_length, hash_number, field_num);
     Recent_Counter CU_Counter(cycle, hash_number * row_length, row_length, hash_number, field_num);
     Recent_Counter CO_Counter(4*cycle/5, hash_number * row_length, row_length, hash_number, field_num);
 
     cout <<"Sliding Sketch,Arrivals,ARE"<<endl;
     int clock_ = clock();
-    // while(fread(packet.str, DATA_LEN, 1, file) > 0)
-    int latestT = indat[0].timestamp;
-	cerr << cycle << endl << sizeof(Data) << endl << latestT<<endl;
-    while(num < input_num_max)
+    while(num < input_num)
     {
         packet = indat[num];
-        if(num > input_num_max){
-            break;
-        }
 #ifndef tpTest
-        // unsigned int pos = num % cycle;
         while(dat.size() && packet.timestamp - dat.front().timestamp >= cycle){
-            mp[dat.front()] -= 1;
+            mp[dat.front()] -= 1;   // expiration of the eldest item in the sliding window
             dat.pop();
         }
 
         dat.push(packet);
-        // dat[pos] = packet;
 #endif
 
 #if !defined(tpTest)||tpTest==1
-        CM_Counter.CM_Init(packet.str, DATA_LEN, packet.timestamp);
+        CM_Counter.CM_Init(packet.str, DATA_LEN, num);
 #endif
 #if !defined(tpTest)||tpTest==2
-        CU_Counter.CU_Init(packet.str, DATA_LEN, packet.timestamp);
+        CU_Counter.CU_Init(packet.str, DATA_LEN, num);
 #endif
 #if !defined(tpTest)||tpTest==3
-        CO_Counter.CO_Init(packet.str, DATA_LEN, packet.timestamp);
+        CO_Counter.CO_Init(packet.str, DATA_LEN, num);
 #endif
 
 #ifndef tpTest
@@ -93,17 +82,13 @@ void Read_File(int argc,char*argv[]){
             mp[packet] = 1;
         else
             mp[packet] += 1;
-        //cerr << packet.timestamp << endl;
+
         if( packet.timestamp - indat[0].timestamp >= 2*cycle){
-            // if(num%(cycle/5) ==0){
             if((int)packet.timestamp - latestT + 1>= cycle/5){ 
                 latestT = packet.timestamp;
                 st.clear();
                 for(auto i = mp.begin(); i!= mp.end();i++){
-			if ( i->second == 0) continue;
-                // for(int i=0;i<cycle;++i){
-                    // if(st.find(dat[i]) != st.end()) continue;
-                    // st.insert(dat[i]);
+					if ( i->second == 0) continue;
                     Test_num ++; 
                     CM_dif = fabs((double)CM_Counter.Query(i->first.str,DATA_LEN) - i->second );
                     CU_dif = fabs((double)CU_Counter.Query(i->first.str,DATA_LEN) - i->second);
@@ -111,7 +96,6 @@ void Read_File(int argc,char*argv[]){
                     CM_ae += CM_dif; CU_ae += CU_dif; CO_ae += CO_dif;
                     CM_re += CM_dif/i->second; CU_re += CU_dif/i->second; CO_re += CO_dif/i->second;
 
-                    // cerr << CM_dif << " "  << mp[dat[i]] <<endl;
                 }
                 
                 // double tp = (double)num /1000000 / ((double)(clock()-clock_)/CLOCKS_PER_SEC);
@@ -121,6 +105,7 @@ void Read_File(int argc,char*argv[]){
                 cerr << "Sl-CM aae" << "," << num << "," << CM_ae / Test_num << endl;
                 cerr << "Sl-CU aae" << "," << num << "," << CU_ae / Test_num << endl;
                 cerr << "Sl-Count aae" << "," << num << "," << CO_ae / Test_num << endl;
+                cerr <<endl;
             }
         }
 
@@ -129,7 +114,8 @@ void Read_File(int argc,char*argv[]){
 
         
     }
-#ifndef tpTest    
+#ifndef tpTest   
+				cout<<"Total"<<endl; 
                 cout << "Sl-CM are" << "," << num << "," << CM_re / Test_num << endl;
                 cout << "Sl-CU are" << "," << num << "," << CU_re / Test_num << endl;
                 cout << "Sl-Count are" << "," << num << "," << CO_re / Test_num << endl;
