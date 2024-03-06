25ddb1192047b0d66167c51e864eccb2a0fc22cb
sliding-sketch <41382295+sliding-sketch@users.noreply.github.com>
2024-03-06 20:22:33 +0800

    Add files via upload

--- src/Frequency/ECM/demo/main.cpp
+++ src/Frequency/ECM/demo/main.cpp
@@ -5,12 +5,11 @@
 #include <time.h>
 #include <unordered_map>
 #include <unordered_set>
-#include "../MMap.h"
 #include "../sketch.cpp"
 #include "../data.cpp"
 #include "../hash_class.cpp"
 
-#define NtpTest 1
+#define NtpTest 1 
 
 using namespace std;
 unordered_map<Data, int, My_Hash> mp;
@@ -29,35 +28,40 @@ unordered_set<Data, My_Hash> st;
 void Read_File(int argc,char*argv[]){
     
     int hash_number = 5;
-	int update_k = atoi(argv[1]);
-	int row_length =  atoi(argv[3]);
+	int update_k = 3;
+	int row_length =  15000;
+	cout<<"memory of ECM approximate to 5MB, but cannot be precisely controlled"<<endl;
 
     // Data *dat = new Data[cycle + 1];
-    int input_num_max =  1000000 * 8;
-    unsigned long long int num = 0;
-    double CM_dif = 0, CU_dif = 0, CO_dif = 0;
-    double CM_ae = 0,  CU_ae = 0,  CO_ae = 0;
-    double CM_re = 0,  CU_re = 0,  CO_re = 0;
-    int Test_num = 0;
-    LoadResult ret = Load(argv[2]);
-    // FILE* file = fopen(argv[2],"rb");
+    FILE* file = fopen("../../../../data/formatted00.dat","rb");
     Data packet;
-    Data* indat = (Data*)(ret.start);
+    vector<Data> indat;
+    unsigned int latestT = 0;
+    while(fread(packet.str, DATA_LEN, 1, file) > 0)
+    {
+    	packet.timestamp = indat.size(); // as the dataset provided in the demo has no timestamp, we use count-based sliding window. In other words, each item will get a timestamp equal to its index in the datasteam, and the length of the
+		// sliding window is equal to the number of items in it. 
+    	indat.push_back(packet);
+	}
+	unsigned int input_num = indat.size();
     queue<Data> dat;
 
-    int cycle = (indat[input_num_max -1].timestamp - indat[0].timestamp ) / 8;
-
+    int cycle = 200000; // the length of the sliding window, here we use count based sliding window.
+	cout<<"window size "<<cycle<<endl; 
+   unsigned long long int num = 0;
+    int Test_num = 0;
+    double CM_dif = 0;
+    double CM_ae = 0;
+    double CM_re = 0;
+    
     ECM ecm(cycle, update_k, hash_number, row_length);
 
     cout <<"ECM,Arrivals,ARE"<<endl;
     int clock_ = clock();
-    // while(fread(packet.str, DATA_LEN, 1, file) > 0)
-    int latestT = indat[0].timestamp;
-	cerr << cycle << endl << sizeof(Data) << endl << latestT<<endl;
-    while(num < input_num_max)
+    while(num < input_num)
     {
         packet = indat[num];
-        if(num > input_num_max){
+        if(num > input_num){
             break;
         }
 #ifndef tpTest
@@ -81,14 +85,11 @@ void Read_File(int argc,char*argv[]){
         if( packet.timestamp - indat[0].timestamp >= 2*cycle){
 		
             // if(num%(cycle/10) ==0){
-            if((int)packet.timestamp - latestT + 1>= cycle/10){ 
+            if((int)packet.timestamp - latestT + 1>= cycle/5){ 
                 latestT = packet.timestamp;
                 st.clear();
                 for(auto i = mp.begin(); i!= mp.end();i++){
 			if ( i->second == 0) continue;
-                // for(int i=0;i<cycle;++i){
-                    // if(st.find(dat[i]) != st.end()) continue;
-                    // st.insert(dat[i]);
                     Test_num ++; 
                     CM_dif = fabs((double)ecm.query(i->first.str,DATA_LEN,packet.timestamp) - i->second );
                     CM_ae += CM_dif; 
@@ -97,9 +98,9 @@ void Read_File(int argc,char*argv[]){
                     // cerr << CM_dif << " "  << mp[dat[i]] <<endl;
                 }
                 
-                cerr << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 << endl;
+                cerr << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 << "MB"<<endl;
                 cerr << "ECM  are" << "," << num<< "," << CM_re / Test_num << endl;
-                cerr << "ECM  are" << "," << num<< "," << CM_ae / Test_num << endl;
+                cerr << "ECM  aae" << "," << num<< "," << CM_ae / Test_num << endl;
             }
         }
 
@@ -109,12 +110,13 @@ void Read_File(int argc,char*argv[]){
         
     }
 #ifndef tpTest 
-		cout << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 << endl;
-                cout << "E-CM are" << "," << num << "," << CM_re / Test_num << endl;
-                cout << "E-CM aae" << "," << num << "," << CM_ae / Test_num << endl;
+		cout<<"total "<<endl;
+		cout << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 <<"MB" <<endl;
+                cout << "ECM are" << "," << num << "," << CM_re / Test_num << endl;
+                cout << "ECM aae" << "," << num << "," << CM_ae / Test_num << endl;
 #else
     double tp = (double)num /1000000 / ((double)(clock()-clock_)/CLOCKS_PER_SEC);
-	cout << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 << endl;
+	cout << "ECM  mem" << "," << num << "," << ecm.q_memory(num)/1024.0/1024.0 << "MB" <<endl;
     cout << "TP" << tpTest << "," << num << "," << tp << endl;
 #endif
 }
